a0e489104d5445c2f1c0ea69cf2f055113bbf4da
Gabe Parmer <gparmer@gwu.edu>
2020-09-21 20:33:24 -0400

    bytecoin dropgit add *git add *

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# fork_comm_join
+# Fork, Communication and Bytecoin Mining
+
+We wrote a program to use many processes to mine bytecoin for us!
+We'll be rich!
+
+Problem is, we have a performance issue.
+We're running the mining in many different processes, and they all finish at different times, but they take forever to get back to us.
+We want to get the bytecoins back *as they are mined*, and don't want to wait for the *next* one to cache in.
+Currently, it does this:
+
+```
+church$ ./bin
+Mining 0 done, cha-ching!
+Mining 1 done, cha-ching!
+Mining 2 done, cha-ching!
+Mining 3 done, cha-ching!
+Mining 4 done, cha-ching!
+Mining 5 done, cha-ching!
+Mining 6 done, cha-ching!
+Mining 7 done, cha-ching!
+Mining 8 done, cha-ching!
+Mining 9 done, cha-ching!
+Mining 10 done, cha-ching!
+Mining 11 done, cha-ching!
+Mining 12 done, cha-ching!
+Mining 13 done, cha-ching!
+Mining 14 done, cha-ching!
+Mining 15 done, cha-ching!
+```
+
+What we want, with non-blocking communication is something more like this:
+```
+church$ ./bin
+Mining 2 done, cha-ching!
+Mining 3 done, cha-ching!
+Mining 10 done, cha-ching!
+Mining 13 done, cha-ching!
+Mining 6 done, cha-ching!
+Mining 8 done, cha-ching!
+Mining 12 done, cha-ching!
+Mining 15 done, cha-ching!
+Mining 11 done, cha-ching!
+Mining 7 done, cha-ching!
+Mining 9 done, cha-ching!
+Mining 1 done, cha-ching!
+Mining 14 done, cha-ching!
+Mining 0 done, cha-ching!
+Mining 5 done, cha-ching!
+Mining 4 done, cha-ching!
+```
+
+- **Question 1:**
+	Why does non-blocking approach give you *bursty* responses?
+	I.e. a bunch cache in at the same time.
+- **Question 2:**
+	What impact will allowing the children to cache out in any order have?
+- **TODO 1:**
+	Change the implementation to use non-blocking communication.
+	Please note that there is already a `nbpipe` that has a non-blocking `read` end.
+- **Question 3:**
+	Is non-blocking faster?
+	In what way?
+	In what way isn't it faster?
+- **TODO 2:**
+	We want this all be *very* fast, so we want to avoid the copies of the pipe-based, message-passing implementation.
+	Please us the shared memory (already set up in `shmem` for each child) to pass the data back to the parent and cash in.
---
+++ main.c
@@ -0,0 +1,169 @@
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <assert.h>
+
+#define NCHILD    16
+#define BYTEMSGSZ 32
+#define RAND_SPAN (RAND_MAX/4)
+
+struct child_data {
+	int   child_off;
+	int   pending;		/* does the child have pending data? */
+	int   pipe[2]; 		/* pipe to send the bytecoin return message to parent. [0] is readable, [1] writable */
+	int   nbpipe[2]; 	/* pipe to send the bytecoin return message to parent with the read-side being non-blocking */
+	char *shmem;		/* shared memory to send the bytecoin return message to parent */
+};
+
+struct child_data children[NCHILD];
+char *shmem;
+
+void
+create_pipes(struct child_data *d)
+{
+	/* d->pipe[0] is the "read" end of the pipe, and d->pipe[1] is for "writes" */
+ 	if (pipe(d->pipe)) {
+		perror("pipe creation error");
+		exit(EXIT_FAILURE);
+	}
+	if (pipe(d->nbpipe)) {
+		perror("nbpipe creation error");
+		exit(EXIT_FAILURE);
+	}
+	if (fcntl(d->nbpipe[0], F_SETFL, O_NONBLOCK)) {
+		perror("nbpipe error setting to non-blocking");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void
+create_shmem(struct child_data *d)
+{
+	assert(shmem != NULL);
+
+	d->shmem = shmem + (d->child_off * BYTEMSGSZ);
+}
+
+void
+ipc_init(void)
+{
+	int i;
+
+	shmem = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+	for (i = 0; i < NCHILD; i++) {
+		struct child_data *c = &children[i];
+
+		c->child_off = i;
+		c->pending   = 1;
+		create_shmem(c);
+		create_pipes(c);
+	}
+}
+
+/*
+ * I hear bitcoin is amazing. And that bytes are greater than
+ * bits. Lets do bytecoin mining!
+ *
+ * TODO 1: update this to send over the non-blocking channel (the
+ * write is still blocking!!!!).
+ *
+ * TODO 2: send the message back in shared memory instead of over a
+ * pipe!
+ */
+void
+bytecoin_mining(struct child_data *us, char byte)
+{
+	char return_message[BYTEMSGSZ];
+	unsigned int i;
+	(void)us;
+
+	/* emulate trying to find a specific byte! */
+	while (rand() % RAND_SPAN != byte) ;
+
+	for (i = 0; i < BYTEMSGSZ; i++) {
+		return_message[i] = byte;
+	}
+
+	/* Add code here to send the return message back to the parent! */
+	write(us->pipe[1], return_message, BYTEMSGSZ);
+
+	return;
+}
+
+int
+main(int argc, char *argv[])
+{
+	int i, msgs_received = 0;
+	time_t t;
+	(void)argc; (void)argv;
+
+	srand((unsigned int)time(&t));
+	ipc_init();
+
+	for (i = 0; i < NCHILD; i++) {
+		switch (fork()) {
+		case -1: {
+			perror("fork failed");
+			exit(EXIT_FAILURE);
+		}
+		case 0: {	/* child */
+			bytecoin_mining(&children[i], i+1);
+			exit(EXIT_SUCCESS);
+		}}
+	}
+
+
+	/*
+	 * Parent logic to understand. Remember your TODOs!
+	 *
+	 * TODO 1: update this to receive over the non-blocking
+	 * channel, the `read` should be non-blocking so that we can
+	 * check for the *next* available bytecoin.
+	 *
+	 * TODO 2: receive the message from a child in shared memory
+	 * instead of over a pipe!
+	 */
+	while (msgs_received < NCHILD) {
+		for (i = 0; i < NCHILD; i++) {
+			char msg[BYTEMSGSZ];
+			int ret;
+
+			/* Skip over the children for which we've already received a message */
+			if (!children[i].pending) continue;
+
+			/* read from the child. */
+			ret = read(children[i].pipe[0], msg, BYTEMSGSZ);
+			if (ret < 0) {
+				if (errno == EAGAIN) {
+					/* non-blocking logic here! */
+					continue; /* Just keep on going through all the children */
+				} else {
+					perror("Parent reading from child");
+					exit(EXIT_FAILURE);
+				}
+			}
+
+			/* We got a message! */
+			assert(ret == BYTEMSGSZ);
+			assert(msg[0] == i+1);
+			children[i].pending = 0;
+			msgs_received++;
+			printf("Mining %d done, cha-ching!\n", i);
+		}
+	}
+
+	for (i = 0; i < NCHILD; i++) {
+		int status;
+
+		wait(&status);
+	}
+        munmap(shmem, 4096);
+
+	return 0;
+}
